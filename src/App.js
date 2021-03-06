d2eb46097efd5997dd2718640ccb6bc8f716af04
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-03-06 20:35:23 +0600

    Create a Componet & Pass Dynamic Data into a Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const person = {
+    name: "Nishat Hasan Muktar",
+    profession: "Now,I'm learnng about React Core Concept",
+  };
+
+  var style = {
+    color: "red",
+    backgroundColor: "white",
+    borderRadius: "10px",
+    boxShadow: "10px 10px 10px black",
+  };
+
+  // --For Componet--//
+  var cricketer = ['Shakib Al Hasan','Tamim Iqbal','Mashrafi','Mushfiqur Rahman','Mahmudullah']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ backgroundColor: "red" }}>
+        
+        {/* <h1>Cricketer List:</h1> */}
+        <Person name={cricketer[0]} status='Alrounder'></Person>
+        <Person name={cricketer[1]} status='Batsman'></Person>
+        <Person name={cricketer[2]} status='Bowler'></Person>
+        <Person name={cricketer[3]} status='Wicket Keeper'></Person>
+        <Person name={cricketer[4]} status='Allrounder'></Person>
+
       </header>
     </div>
+  );
+}
+
+// --Declare or create a component using function--//
+
+function Person(props){
+
+  // --Creating a object for CSS style --//
+  var style={
+    border:'2px solid yellow',
+    margin:'20px',
+    padding:'10px'
+  }
+  
+  return(
+<div style={style}>
+  <h1>Name : {props.name}</h1>
+  <h2>Status : {props.status}</h2>
+</div>
   );
 }
 
